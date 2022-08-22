a1c6e7f2370231af7ea7962482acfb8505658b2a
Guilherme Afonso <gasp@cin.ufpe.br>
2022-08-22 00:24:32 -0300

    fix: remove unused method

--- Projeto/backend/src/facades/facade.ts
+++ Projeto/backend/src/facades/facade.ts
@@ -55,31 +55,4 @@ export class Facade {
 	async createChallenge(challenge: Challenge) {
 		return await this.challengeControl.createChallenge(challenge);
 	}
-
-	async handleRequest<T>(operationType: OperationType, data: T) {
-		switch (operationType) {
-			case OperationType.GET_USER:
-				return await this.userControl.getUser<T>(data);
-			case OperationType.GET_USERS:
-				return await this.userControl.getUsers<T>(data);
-			case OperationType.CREATE_USER:
-				return await this.userControl.createUser<T>(data);
-			case OperationType.UPDATE_USER:
-				return await this.userControl.updateUser<T>(data);
-			case OperationType.DELETE_USER:
-				return await this.userControl.deleteUser<T>(data);
-			case OperationType.AUTH_CODE_GRANT:
-				return await this.authControl.getCredentials<T>(data);
-			case OperationType.REFRESH_TOKEN:
-				return await this.authControl.refreshToken<T>(data);
-			case OperationType.AUTHORIZE_URL:
-				return await this.authControl.getAuthorizeUrl<T>(data);
-			case OperationType.CREATE_CHALLENGE:
-				return await this.challengeControl.createChallenge<T>(data);
-			case OperationType.LOGIN:
-				return await this.authControl.login<T>(data);
-			default:
-				break;
-		}
-	}
 }
