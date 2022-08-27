e419578139d0ee7bdfe7704445cfe74271bfd5c1
Guilherme Afonso <gasp@cin.ufpe.br>
2022-08-27 01:13:33 -0300

    fix: update login page to use auth service

--- Projeto/web-app/src/app/login/login.page.ts
+++ Projeto/web-app/src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { AuthService } from '../musa-services/auth.service';
 import { EndpointService } from '../musa-services/endpoint.service';
 
 @Component({
@@ -9,31 +10,35 @@ import { EndpointService } from '../musa-services/endpoint.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
   hasCodeInParams: boolean;
   spotifyAuthorizationUrl: string;
 
-  constructor(private router: Router, private route: ActivatedRoute, private endpointService: EndpointService) {
-    const params = new URLSearchParams({
-      ['client_id']: environment.spotifyClientId,
-      ['response_type']: 'code',
-      ['redirect_uri']: environment.redirectUri,
-      ['scope']: 'user-read-recently-played user-read-currently-playing user-read-private user-read-email'
-    });
-
-    this.spotifyAuthorizationUrl = `https://accounts.spotify.com/authorize?${params.toString()}`;
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(async params => {
+    this.getAuthorizeUrl();
+    this.route.queryParams.subscribe(async (params) => {
       if (params.code) {
         this.hasCodeInParams = true;
-        await this.endpointService.login(params.code);
-        setTimeout(() => { this.router.navigate(['home']); }, 300);
+        const tokens = await this.authService.codeGrant(params.code);
+        await this.authService.login(tokens);
+        setTimeout(() => {
+          this.router.navigate(['home']);
+        }, 300);
       } else {
         this.hasCodeInParams = false;
       }
     });
   }
 
+  async getAuthorizeUrl() {
+    try {
+      const request = await this.authService.getAuthorizeUrl();
+      this.spotifyAuthorizationUrl = request as string;
+    } catch (error) {}
+  }
 }
